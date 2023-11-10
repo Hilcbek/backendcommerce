71d5c30d0a3f4930b5ecbe926522e33bd1fe1f07
Hilcbek <balemayehu07@gmail.com>
2023-11-10 21:18:56 +0300

    added

--- routers/product.router.js
+++ routers/product.router.js
@@ -2,9 +2,9 @@ import express from 'express'
 import { AddProduct, AllProduct, GetCatagory, GetCatagoryByCatandColor, SingleProduct, getRandomData } from '../controllers/product.controller.js'
 import { verifyIsUserAdmin, verifyUserILoggedInForBackend } from '../Token/token.js'
 export let productRouter = express.Router()
-productRouter.post('/add',verifyIsUserAdmin,AddProduct)
-productRouter.get('/single/:id',verifyUserILoggedInForBackend,SingleProduct)
+productRouter.post('/add',AddProduct)
+productRouter.get('/single/:id',SingleProduct)
 productRouter.get('/',AllProduct)
 productRouter.get('/random',getRandomData)
-productRouter.get('/catagory/:cat',verifyUserILoggedInForBackend,GetCatagory)
-productRouter.get('/catagory/bothcat/pro',verifyUserILoggedInForBackend,GetCatagoryByCatandColor)
+productRouter.get('/catagory/:cat',GetCatagory)
+productRouter.get('/catagory/bothcat/pro',GetCatagoryByCatandColor)
